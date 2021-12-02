01285ba1d2d84b96811da7559dfbcefca9fd747a
Jawsen <72077207+Jawsen@users.noreply.github.com>
2021-12-02 15:25:03 +0100

    tilføjet point og at man ka miste og få liv

--- app/src/main/java/com/example/s205414lykkehjulet/GameFragment.kt
+++ app/src/main/java/com/example/s205414lykkehjulet/GameFragment.kt
@@ -9,7 +9,9 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import java.lang.Math
+import java.lang.reflect.Constructor
 import kotlin.random.Random
 
 
@@ -29,6 +31,15 @@ class GameFragment : Fragment() {
     private var param2: String? = null
     private var hemmeligOrd = ""
     private var spillersGæt = ""
+    private var spilvundet: Boolean = false
+    private var spiltabt: Boolean = false
+    private var snurHjul = 0
+    private var pointGivet = 0
+    private var livTilbage = 5
+    private var pointGemt = 0
+    private var spillerPoint = 0
+    private var tilfældigtFelt = ""
+
 
     // ord der bliver displayet i vistOrd
     private var dOrd = ""
@@ -54,17 +65,23 @@ class GameFragment : Fragment() {
         val gætKnap = view.findViewById<Button>(R.id.gætKnap)
         val gætteOmråde = view.findViewById<EditText>(R.id.gætteOmråde)
         val vistOrd = view.findViewById<TextView>(R.id.visOrd)
-
+        val liv = view.findViewById<TextView>(R.id.liv)
+        val displayAfhjul = view.findViewById<TextView>(R.id.displayAfhjul)
+        val point = view.findViewById<TextView>(R.id.point)
 
 
         startKnap.setOnClickListener {
             startKnap.visibility = View.INVISIBLE
             drejKnap.visibility = View.VISIBLE
-            gætKnap.visibility = View.VISIBLE
             vistOrd.visibility = View.VISIBLE
-            gætteOmråde.visibility = View.VISIBLE
+            displayAfhjul.visibility = View.VISIBLE
+            velkommenText.visibility = View.INVISIBLE
+            point.visibility = View.VISIBLE
+            liv.visibility = View.VISIBLE
+
 
             startSpil()
+            liv.setText(livTilbage.toString())
             vistOrd.text = dOrd
             Toast.makeText(activity, hemmeligOrd, Toast.LENGTH_SHORT).show()
 
@@ -72,17 +89,42 @@ class GameFragment : Fragment() {
         }
 
         drejKnap.setOnClickListener {
-
+            gætKnap.visibility = View.VISIBLE
+            drejKnap.visibility = View.INVISIBLE
+            gætteOmråde.visibility = View.VISIBLE
+            displayAfhjul.visibility = View.VISIBLE
+            val hjulSnurrer = context?.resources?.getStringArray(R.array.point)
+            tilfældigtFelt = hjulSnurrer?.random().toString()
+            displayAfhjul.setText(tilfældigtFelt)
             Toast.makeText(activity, "DU HAR TRYKKET DREJ!!!!", Toast.LENGTH_SHORT).show()
+            spinHjul()
+            liv.setText(livTilbage.toString())
 
 
         }
         gætKnap.setOnClickListener {
+            gætKnap.visibility = View.INVISIBLE
+            drejKnap.visibility = View.VISIBLE
+            gætteOmråde.visibility = View.INVISIBLE
+            displayAfhjul.visibility = View.INVISIBLE
             spillersGæt = gætteOmråde.text.toString().toLowerCase()
             gætteOmråde.text = null
             tjekGæt()
+            spinHjul()
+            liv.setText(livTilbage.toString())
+            point.setText(spillerPoint.toString())
             vistOrd.text = dOrd
-            //displayBogstavet("")
+            println(spilvundet)
+            println(hemmeligOrd)
+            println(dOrd)
+            if (spilvundet) {
+                Navigation.findNavController(view).navigate(R.id.action_goToWin)
+            }
+            if (spiltabt) {
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_gameFragment_to_loseFragment)
+            }
+
             Toast.makeText(activity, "Du har trykket på gæt", Toast.LENGTH_SHORT).show()
 
         }
@@ -105,6 +147,7 @@ class GameFragment : Fragment() {
 
     }
 
+    // funktion der tjekker om spillers gæt er rigtig
     private fun tjekGæt() {
         if (spillersGæt.length == 1) {
             if (spillersGæt in hemmeligOrd.toLowerCase() && spillersGæt !in dOrd) {
@@ -112,55 +155,119 @@ class GameFragment : Fragment() {
                 rigtigGæt.add(spillersGæt)
                 displayBogstavet()
 
-
-
+                spillerPoint += pointGivet
                 Toast.makeText(activity, "God gættet", Toast.LENGTH_SHORT).show()
-                //checkWin()
+                tjekVinder()
+                //tjekTabtSpil()
+
                 return
-                Toast.makeText(activity, "Du har tastet mere end et bogstav", Toast.LENGTH_SHORT).show()
+
+            } else {
+                livTilbage-=1
             }
         }
 
     }
 
+
+    // funktion der displayer rigitg gættet bogstav
     private fun displayBogstavet() {
-        val indexAfBogstav = hemmeligOrd.indexesOf(spillersGæt,true)
+        val indexAfBogstav = hemmeligOrd.indexesOf(spillersGæt, true)
 
-        if(indexAfBogstav.size ==1){
-            dOrd = dOrd.replaceRange(indexAfBogstav[0], indexAfBogstav[0] +1, spillersGæt)
+        if (indexAfBogstav.size == 1) {
+            dOrd = dOrd.replaceRange(indexAfBogstav[0], indexAfBogstav[0] + 1, spillersGæt)
 
-        } else if (indexAfBogstav.size == 2){
-            dOrd = dOrd.replaceRange(indexAfBogstav[0], indexAfBogstav[0] +1, spillersGæt)
-            dOrd = dOrd.replaceRange(indexAfBogstav[1], indexAfBogstav[1] +1, spillersGæt)
+        } else if (indexAfBogstav.size == 2) {
+            dOrd = dOrd.replaceRange(indexAfBogstav[0], indexAfBogstav[0] + 1, spillersGæt)
+            dOrd = dOrd.replaceRange(indexAfBogstav[1], indexAfBogstav[1] + 1, spillersGæt)
         }
 
 
         println(dOrd)
-        /*
-        while (indexAfBogstav>=0){
-            dOrd = indexAfBogstav
-        }
-            hemmeligOrd[indexAfBogstav] = spillersGæt.toCharArray(indexAfBogstav)
-        }
 
-         */
-        /*if (hemmeligOrd.contains(spillersGæt.toLowerCase())) {
-           dOrd = ind
-        } else {
-
-        }
-      return
-    }*/
     }
 
-    //
+    fun tjekVinder() {
+        if (hemmeligOrd.lowercase() == dOrd) {
+            Toast.makeText(activity, "YAAAAY!!! du vandt", Toast.LENGTH_SHORT).show()
+            spilvundet = true
+
+        }
+    }
+
+
+    fun spinHjul() {
+        if (tilfældigtFelt.equals("Du har får 100 point hvis du gætter rigtigt")) {
+            pointGivet = 100
+            //point.setText(pointGemt.toString())
+            println(pointGemt)
+
+
+        }
+        if (tilfældigtFelt.equals(("Du har får 200 point hvis du gætter rigtigt"))) {
+            pointGivet = 200
+            //point.setText(pointGemt.toString())
+            println(pointGemt)
+
+
+        }
+        if (tilfældigtFelt.equals(("Du har får 1000 point hvis du gætter rigtigt"))) {
+            pointGivet = 1000
+
+            println(pointGemt)
+
+
+        }
+        if (tilfældigtFelt.equals(("Du har får 2000 point hvis du gætter rigtigt"))) {
+            pointGivet = 2000
+            println(pointGemt)
+
+
+        }
+        if (tilfældigtFelt.equals(("Du har fået et ekstra liv"))){
+            livTilbage +=1
+
+        }
+
+        if (tilfældigtFelt.equals(("Du har mistet et liv "))){
+            livTilbage -=1
+
+        }
+
+        if (tilfældigtFelt.equals(("Du er gået bankerot "))){
+
+        }
+
+
+        // gør brug af .setText(point.toString())
+    }
+
+
+    // fra internettet https://stackoverflow.com/questions/62189457/get-indexes-of-substrings-contained-in-a-string-in-kotlin-way?fbclid=IwAR0ADek0xyM8CCIagLT1C_7VdZuNZQp6utCf4FI1D_wg1yFjPPW0Bpbarzo
 
     fun ignoreCaseOpt(ignoreCase: Boolean) =
         if (ignoreCase) setOf(RegexOption.IGNORE_CASE) else emptySet()
 
     fun String?.indexesOf(pat: String, ignoreCase: Boolean = true): List<Int> =
-        pat.toRegex(ignoreCaseOpt(ignoreCase)).findAll(this?: "").map { it.range.first }.toList()
+        pat.toRegex(ignoreCaseOpt(ignoreCase)).findAll(this ?: "").map { it.range.first }.toList()
+
+
 }
+   /* fun tjekTabtSpil(){
+        if (spillersGæt !in hemmeligOrd){
+            livTilbage--
+            if(livTilbage <= 0){
+                spiltabt = true
+
+            }
+        }
+        if (pointGemt <= 0){
+            Toast.makeText(activity, "DU ER gået bankerot", Toast.LENGTH_SHORT).show()
+            spiltabt = true
+
+        }
+    }*/
+
 
 
 
